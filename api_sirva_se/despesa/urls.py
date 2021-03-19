faf7ef9af4f35a51b5ea582adba85cf2493cbdf5
Filipe Neiva <filipevilanovaneiva@gmail.com>
2021-03-19 12:38:29 -0300

    get despesas

--- api_sirva_se/despesa/urls.py
+++ api_sirva_se/despesa/urls.py
@@ -1,3 +1,5 @@
 from rest_framework import routers
+from despesa.views import DespesaList
 
 router = routers.DefaultRouter()
+router.register(r'', DespesaList, basename='despesas')
--- api_sirva_se/despesa/views.py
+++ api_sirva_se/despesa/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from oauth2_provider.contrib.rest_framework import TokenHasReadWriteScope
+from despesa.serializers import DespesaSerializer
+from despesa.models import Despesa
+from api_sirva_se.utils import pegar_usuario_por_token
 
 # Create your views here.
+
+class DespesaList(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticated, TokenHasReadWriteScope]
+    serializer_class = DespesaSerializer
+
+    def get_queryset(self):
+        app_tk = self.request.META["HTTP_AUTHORIZATION"]
+        user  = pegar_usuario_por_token(app_tk)
+
+        return Despesa.objects.filter(mercearia=user)
+    
